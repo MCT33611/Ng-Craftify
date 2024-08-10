16d2fceb6d7998f47ef835d4129e6b13880a3a95
muhammed fahmi <muhammedfahmi04@gmail.com>
2024-08-10 13:57:42 +0530

    readiy production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
+    production: true,
+    API_BASE_URL: 'https://localhost:7283',
+    GoogleClientId: '689816043988-mjsct2n5ngoff5pf4isj3cu8f1sm5aip.apps.googleusercontent.com',
+    RazorpaySettings: {
+        "KeyId": "rzp_test_G0Bly61lW4bqO7",
+        "KeySecret": "QhQ3TUJPOMCUnDV0RfIrgzb0"
+    }
 };
-  
